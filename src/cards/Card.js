bfbd771723c10e189aba4ab9eafdce6f6ac59dce
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-12-04 08:38:51 -0800

    Change Card to functional component.

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CardControls from './CardControls';
 
 const Card = ({ card, incrementCount, show, showCard, handleUpdate }) => {
